7dcb0aa53733fa50a9b17c322c688ecb62bf6242
jochemthart1 <75669163+jochemthart1@users.noreply.github.com>
2021-01-25 13:04:13 +0100

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,6 @@
+# Geoscripting 2020 
+# Title: Corona Locator
+# Team: Baguette (Hugo Poupard & Jochem 't Hart)
+# Final Project
+# Date: 29-01-2021
+
---
+++ main.py
@@ -0,0 +1,5 @@
+# Geoscripting 2020 
+# Title: Corona Locator
+# Team: Baguette (Hugo Poupard & Jochem 't Hart)
+# Final Project
+# Date: 29-01-2021
